5b182451734449bd7b4f1631a9863d164d101dfb
Ali Sulman <alithedeveloper1470@gmail.com>
2025-01-28 23:24:11 +0500

    clone nextjs and wix template repo

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+export default {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.wixstatic.com'
+      }
+    ]
+  }
+};
