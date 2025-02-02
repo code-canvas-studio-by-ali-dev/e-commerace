ee3ca2ca87970651ff51d059eefe8bcc0664788d
Ali Sulman <alithedeveloper1470@gmail.com>
2025-02-02 14:01:48 +0500

    work done by ali sulman #020220251302

---
+++ README.md
@@ -0,0 +1,11 @@
+*Hi there!* 👋  
+
+I’m interested in buying *product* from your store.  
+
+> Description:
+ Color: 
+ Size:
+
+Can you please share more details?  
+
+Thank you! 😊
--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -7,12 +7,27 @@ import { Button } from '@headlessui/react';
 import { PhoneIcon } from '@heroicons/react/24/outline';
 
 export function ProductDescription({ product }: { product: Product }) {
+  const generateWhatsappMessage = (product: Product) => {
+    const message = `
+    *Hello there!*  
+    I’m interested in buying *${product.title}* from your store.
+    *Can you please share more details?*  
+    Looking forward to your response! 
+    *Thank you!*  
+    `;
+    const encodedMessage = encodeURIComponent(message);
+    return `https://wa.me/923474732981?text=${encodedMessage}`;
+  }
+  const handleClick = () => {
+    const whatsappLink = generateWhatsappMessage(product)
+    window.open(whatsappLink, '_blank');
+  }
   return (
     <>
       <div className="mb-6 flex flex-col border-b pb-6 dark:border-neutral-700">
         <h1 className="mb-4 text-4xl font-medium">{product.title}</h1>
         <div className="mr-auto w-auto rounded-full bg-blue-600 p-2 text-sm text-white">
-          <Button className={"flex items-center gap-2 text-xs md:text-sm px-3"}>
+          <Button className={"flex items-center gap-2 text-xs md:text-sm px-3"} onClick={() => handleClick()}>
             <PhoneIcon className='size-5' />
             Get mote info
           </Button>
